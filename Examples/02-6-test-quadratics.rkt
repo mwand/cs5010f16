e3b56e1cfd1f7c87854a607720235b9d5e4f052c
Mitch Wand <wand@lenovo-2015>
2016-09-20 23:17:56 -0400

    fixed bug in Examples/02-6-test-quadratics.rkt

--- Examples/02-6-test-quadratics.rkt
+++ Examples/02-6-test-quadratics.rkt
@@ -1,13 +1,16 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname 02-6-test-quadratics) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
 (require rackunit)
 (require "extras.rkt")
 
 ;; zero-of-quadratic? : Real^4 -> Boolean
 ;; RETURNS: whether ax^2 + bx + c < tolerance
 (define (zero-of-quadratic? a b c x tol)
-  (< (+ (* a x x)
-       (* b x)
-       c)
-    tol))
+  (< (abs (+ (* a x x)
+             (* b x)
+             c))
+     tol))
 
 (define (quadratic-solution1 a b c)
   (/ (+ (- b) (sqrt (- (* b b) (* 4 a c))))
@@ -18,11 +21,10 @@
     (* 2 a)))
 
 (begin-for-test
-  (check-true (zero-of-quadratic? 1 0 4 (quadratic-solution1 1 0 4)))
-  (check-true (zero-of-quadratic? 1 0 4 (quadratic-solution2 1 0 4)))
-  (check-true (zero-of-quadratic? 1 0 1 (quadratic-solution1 1 0 1)))
-  (check-true (zero-of-quadratic? 1 0 1 (quadratic-solution2 1 0 1)))
-  (check-true (zero-of-quadratic? 13 58 6 (quadratic-solution1 3 5 6))))
+  (check-true (zero-of-quadratic? 1 0 4 (quadratic-solution1 1 0 4) .01))
+  (check-true (zero-of-quadratic? 1 0 4 (quadratic-solution2 1 0 4) .01))
+  (check-true (zero-of-quadratic? 13 58 6 (quadratic-solution1 13 58 6) .01))
+  )
 
 
 
